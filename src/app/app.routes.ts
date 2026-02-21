8fa7127b1bdc9aef9d0426f9ea0108583ae92186
Sodiq Abdullah <148208065+SodiqAbdullah@users.noreply.github.com>
2026-02-21 23:13:34 +0700

    reset

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: '',
-    // File routes ini ada di src/app/, jadi path ke home cukup ./home/home.page
-    loadComponent: () => import('./home/home.page').then(m => m.HomePage),
-  },
-];
